e05e6dbf1766c4e48ef661f34b3d767b3a82f936
Zelalem Mekonen <zola@programmer.net>
2021-12-06 16:11:49 +0300

    added total count to top

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,45 +1,112 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { useSubscription, useResult } from '@vue/apollo-composable'
 import people_by_country from '@/queries/people_by_country.gql'
+import total from '@/queries/total.gql'
+import { format } from 'd3-format'
+import _map_keys from 'lodash.mapkeys'
+import _pad from 'lodash.padstart'
+import VueNumberAnimation from 'vue-number-animation/Number.vue'
 
 const { result, loading } = useSubscription(people_by_country, null, {
-  fetchPolicy: "no-cache"
+  fetchPolicy: "network-only"
 });
+
+let { onResult: onTotal } = useSubscription(total, null, {
+  fetchPolicy: "network-only"
+})
 
 const people = useResult(result, [], data => data.people_by_country)
 
+const people_count_map = computed(() => {
+  return _map_keys(people.value, "country_id")
+})
+
+const f = format("~s")
+
+const legend = ref(false)
+
+const last_total = ref(0);
+
+const new_total = ref(0);
+
+const current_country = ref(undefined);
+
+let first_load = true;
+
+const hover_country = (data) => {
+  legend.value = true
+  current_country.value = {
+    has_data: people_count_map.value[data.code] !== undefined,
+    stat: people_count_map.value[data.code],
+    data
+  }
+}
+
+const leave_country = () => {
+  legend.value = false
+  current_country.value = undefined
+}
+
+onTotal(({ data }) => {
+  first_load ? last_total.value = 0 : last_total.value = new_total.value
+  first_load = false
+  new_total.value = data.people_aggregate.aggregate.count
+})
+
+const format_count = (number) => {
+  return _pad(number.toFixed(0), 7, '0')
+}
 </script>
 
 <template>
-<div class="flex h-full">
-  <div class="flex-1 shadow rounded-lg m-4 p-4">
-    <world-map/>
-  </div>
-  <div class="max-w-sm flex-1 m-4 p-4 shadow rounded-lg">
-    <h1 class="font-bold text-2xl text-center mb-2 text-pale-sky-500">Top Countries</h1>
-    <ul role="list" class="-my-5 divide-y divide-pale-sky">
-      <li v-for="p in people" :key="p.country_id" class="py-4">
-        <div class="flex items-center space-x-4">
-          <div class="flex-shrink-0 text-3xl">
-            {{ p.country.icon }}
+  <div class="flex flex-wrap h-full">
+    <div class="flex-1 relative m-4 p-4">
+      <div class="text-center mb-4">
+        <vue-number-animation
+          class="text-pale-sky font-mono font-extrabold text-4xl"
+          ref="number"
+          :from="last_total"
+          :duration="2"
+          :to="new_total"
+          :format="format_count"
+        />
+      </div>
+      <world-map
+        :people_by_country="people_count_map"
+        @hovercountry="hover_country"
+        @hoverleavecountry="leave_country"
+        class="h-full w-full"
+      />
+      <div
+        class="bg-white absolute shadow-2xl px-8 py-4 text-center rounded-lg"
+        v-if="legend && current_country.has_data"
+        :style="{ left: current_country.data.position.left + 'px', top: current_country.data.position.top + 'px' }"
+      >
+        <h2
+          class="text-2xl text-pale-sky font-bold"
+        >{{ current_country.stat.country.icon }} {{ current_country.stat.country.name }}</h2>
+        <h3 class="text-xl text-pale-sky-100 font-semibold">{{ current_country.stat.count }}</h3>
+      </div>
+    </div>
+    <div class="max-w-sm flex-1 m-4 p-4 shadow rounded-lg">
+      <h1 class="font-bold text-2xl text-center mb-2 text-pale-sky-500">Top Countries</h1>
+      <ul class="divide-y divide-pale-sky">
+        <li v-for="p in people" :key="p.country_id" class="py-4">
+          <div class="flex items-center space-x-4">
+            <div class="flex-shrink-0 text-3xl">{{ p.country.icon }}</div>
+            <div class="flex-1 min-w-0">
+              <p class="text-sm font-medium text-pale-sky-600 truncate">{{ p.country.name }}</p>
+              <p class="text-sm text-pale-sky truncate">{{ p.country.native }}</p>
+            </div>
+            <div>
+              <span
+                class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-pale-sky text-sm leading-5 font-medium rounded-full text-pale-sky-900"
+              >{{ f(p.count) }}</span>
+            </div>
           </div>
-          <div class="flex-1 min-w-0">
-            <p class="text-sm font-medium text-pale-sky-600 truncate">
-              {{ p.country.name }}
-            </p>
-            <p class="text-sm text-pale-sky truncate">
-              {{ p.country.native }}
-            </p>
-          </div>
-          <div>
-            <span class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-pale-sky text-sm leading-5 font-medium rounded-full text-pale-sky-900">
-               {{ p.count }}
-            </span>
-          </div>
-        </div>
-      </li>
-      </ul> 
-  </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
